a2462d399f233e6a7fa1097dce813831d094d68b
David Gus Shields <dgs84@pm.me>
2023-09-05 03:45:48 -0500

    fix: checkhealth warn and error

--- lua/nvim-dap-repl-highlights/health.lua
+++ lua/nvim-dap-repl-highlights/health.lua
@@ -5,14 +5,14 @@ local utils = require("nvim-dap-repl-highlights.utils")
 
 function M.check()
 	if vim.fn.has("nvim-0.9.0") == 0 then
-		vim.health.report_error("nvim-dap-repl-highlights requires neovim version >= 0.9.0")
+		vim.health.error("nvim-dap-repl-highlights requires neovim version >= 0.9.0")
 		return
 	end
 
 	if utils.check_treesitter_parser_exists(rl.PARSER_NAME) then
 		vim.health.ok("")
 	else
-		vim.health.report_warn(rl.PARSER_NAME .. " parser not installed", "Run TSInstall " .. rl.PARSER_NAME)
+		vim.health.warn(rl.PARSER_NAME .. " parser not installed", "Run TSInstall " .. rl.PARSER_NAME)
 	end
 end
 
