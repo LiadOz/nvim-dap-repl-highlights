be9c236940109de71e3c152ebff60385c1bfe0f8
David Gus Shields <dgs84@pm.me>
2023-09-05 03:39:40 -0500

    fix: deprecated checkhealth function

--- lua/nvim-dap-repl-highlights/health.lua
+++ lua/nvim-dap-repl-highlights/health.lua
@@ -1,20 +1,19 @@
 local M = {}
 
-local rl = require('nvim-dap-repl-highlights')
-local utils = require('nvim-dap-repl-highlights.utils')
+local rl = require("nvim-dap-repl-highlights")
+local utils = require("nvim-dap-repl-highlights.utils")
 
 function M.check()
-  if vim.fn.has('nvim-0.9.0') == 0 then
-    vim.health.report_error("nvim-dap-repl-highlights requires neovim version >= 0.9.0")
-    return
-  end
+	if vim.fn.has("nvim-0.9.0") == 0 then
+		vim.health.report_error("nvim-dap-repl-highlights requires neovim version >= 0.9.0")
+		return
+	end
 
-  if utils.check_treesitter_parser_exists(rl.PARSER_NAME) then
-    vim.health.report_ok("")
-  else
-    vim.health.report_warn(rl.PARSER_NAME .. " parser not installed", "Run TSInstall " .. rl.PARSER_NAME)
-  end
+	if utils.check_treesitter_parser_exists(rl.PARSER_NAME) then
+		vim.health.ok("")
+	else
+		vim.health.report_warn(rl.PARSER_NAME .. " parser not installed", "Run TSInstall " .. rl.PARSER_NAME)
+	end
 end
-
 
 return M
