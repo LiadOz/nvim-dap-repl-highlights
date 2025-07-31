4f4cdb9ccd567512fd4f602e411b8f7ab5f69002
Liad Oz <liadozil@gmail.com>
2025-07-31 18:58:26 +0300

    fix(init): use treesitter get_lang instead of nvim-treesitter version for compatability with the main branch

--- lua/nvim-dap-repl-highlights/init.lua
+++ lua/nvim-dap-repl-highlights/init.lua
@@ -11,7 +11,7 @@ function M.get_repl_lang_for_session(session)
   end
   local lang = session.config and session.config.repl_lang
   if lang == nil then
-    local ok, ts_lang = pcall(require("nvim-treesitter.parsers").ft_to_lang, session.filetype)
+    local ok, ts_lang = pcall(vim.treesitter.language.get_lang, session.filetype)
     if ok then lang = ts_lang end
   end
   if not lang then -- allow user to provide empty string to suppress this message
